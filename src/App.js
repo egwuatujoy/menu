9090634c78bf90606ff4cf29b4109303dacbe753
joyegwuatu <egwuatujoy60@gmail.com>
2025-02-27 09:56:12 +0100

    updated files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Nav from "./components/Nav";
 import Homepage from "./components/Homepage";
+import Menus from "./components/Menus";
 const App = () => {
   return (
     <div>
       <Nav />
       <Homepage />
+      <Menus />
     </div>
   );
 };
---
+++ src/components/Menus.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Menus = () => {
+  return (
+    <div className='menus'>
+      <h1>Our menu</h1>
+    </div>
+  )
+}
+
+export default Menus
