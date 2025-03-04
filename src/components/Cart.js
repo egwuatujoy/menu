cdaee2909588d8c1ebe47d5c3a9a1d523f2a9f0a
joyegwuatu <egwuatujoy60@gmail.com>
2025-03-04 22:12:29 +0100

    updated files

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,11 +6,19 @@ const Cart = ({ open, cartItems }) => {
       <h1>Cart</h1>
       {cartItems.length > 0 ? (
         cartItems.map((item, index) => (
-          <div key={index} className="cart-item">
-            <img src={item.image} alt={item.name} />
-            <h2>{item.name}</h2>
-            <p>{item.timeToMake} mins</p>
-            <h3>${item.price}</h3>
+          <div className="cart-div">
+            <div key={index} className="cart-item">
+              <img src={item.image} alt={item.name} />
+              <h2>{item.name}</h2>
+              <p>{item.timeToMake} mins</p>
+              <h3>${item.price}</h3>
+            </div>
+
+            <div className="cart-button">
+              <button>+</button>
+              <span>0</span>
+              <button>-</button>
+            </div>
           </div>
         ))
       ) : (
